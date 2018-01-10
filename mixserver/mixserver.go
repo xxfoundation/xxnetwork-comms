b90ab4bcae3ac66dfd99e1c662e55a52f2bcf81b
Richard T. Carback III <rick.carback@gmail.com>
2018-01-10 13:14:28 -0800

    start a dummy server on port 5555

---
+++ mixserver/mixserver.go
@@ -0,0 +1,40 @@
+// mixserver.go - Send/Receive functions for cMix servers
+//
+// Copyright © 2018 Privategrity Corporation
+// All rights reserved.
+
+package mixserver
+
+import (
+	"log"
+	"net"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	pb "gitlab.com/privategrity/comms/mixmessages"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (
+	*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+func StartServer(port string) {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterMixMessageServiceServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
---
+++ mixserver/mixserver_test.go
@@ -0,0 +1,14 @@
+// Copyright © 2018 Privategrity Corporation
+//
+// All rights reserved.
+
+package mixserver
+import (
+
+	"testing"
+)
+
+func TestStartServer(t *testing.T) {
+	// Uncomment the following to start a server on tests
+	// StartServer("localhost:5555")
+}
