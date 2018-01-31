07a265de3ebbeda8d19a138edd074958930c25ac
Spencer Brown <spencer@privategrity.com>
2018-01-31 11:44:50 -0800

    Add send precomp decrypt function

--- mixserver/mixserver.go
+++ mixserver/mixserver.go
@@ -48,14 +48,13 @@ func (s *server) AskOnline(ctx context.Context, msg *pb.Ping) (
 }
 
 // Send an AskOnline message to a particular server
-func (s *server) SendAskOnline(addr string, message *pb.Ping) (*pb.Pong, error) {
+func SendAskOnline(addr string, message *pb.Ping) (*pb.Pong, error) {
 	conn, err := grpc.Dial(addr, grpc.WithInsecure())
 
 	if err != nil {
 		jww.ERROR.Printf("Failed to connect to server with address %s",
 			addr)
 	}
-	// Removed a sleep here
 
 	c := pb.NewMixMessageServiceClient(conn)
 	// Send AskOnline Request and check that we get an AskOnlineAck back
@@ -77,6 +76,57 @@ func (s *server) PrecompDecrypt(ctx context.Context, msg *pb.PrecompDecryptMessa
 	// Call the server handler with the msg
 	serverHandler.PrecompDecrypt(msg)
 	return &pb.Ack{}, nil
+}
+
+func SendPrecompDecrypt(nextServer string, input *pb.PrecompDecryptMessage) (*pb.Ack, error) {
+	// Create Dispatcher for Decrypt
+	dcPrecompDecrypt := services.DispatchCryptop(node.Grp, precomputation.Decrypt{}, nil, nil, round)
+
+	// Convert input message to equivalent SlotDecrypt
+	slotDecrypt := &precomputation.SlotDecrypt{
+		Slot:                         input.Slot,
+		EncryptedMessageKeys:         cyclic.NewIntFromBytes(input.EncryptedMessageKeys),
+		EncryptedRecipientIDKeys:     cyclic.NewIntFromBytes(input.EncryptedRecipientIDKeys),
+		PartialMessageCypherText:     cyclic.NewIntFromBytes(input.PartialMessageCypherText),
+		PartialRecipientIDCypherText: cyclic.NewIntFromBytes(input.PartialRecipientIDCypherText),
+	}
+	// Type assert SlotDecrypt to Slot
+	var slot services.Slot = slotDecrypt
+
+	// Pass slot as input to Decrypt
+	dcPrecompDecrypt.InChannel <- &slot
+
+	// Get output from Decrypt
+	output := <-dcPrecompDecrypt.OutChannel
+	// Type assert Slot to SlotDecrypt
+	out := (*output).(*precomputation.SlotDecrypt)
+
+	// Attempt to connect to nextServer
+	conn, err := grpc.Dial(nextServer, grpc.WithInsecure())
+	// Check for an error
+	if err != nil {
+		jww.ERROR.Printf("Failed to connect to server at %v\n", nextServer)
+	}
+
+	// Prepare to send a message
+	c := pb.NewMixMessageServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+	defer cancel()
+
+	// Send the PrecompDecrypt message using the Decrypt output
+	result, err = c.PrecompDecrypt(ctx, &pb.PrecompDecryptMessage{
+		Slot:                         out.Slot,
+		EncryptedMessageKeys:         out.EncryptedMessageKeys.Bytes(),
+		EncryptedRecipientIDKeys:     out.EncryptedRecipientIDKeys.Bytes(),
+		PartialMessageCypherText:     out.PartialMessageCypherText.Bytes(),
+		PartialRecipientIDCypherText: out.PartialRecipientIDCypherText.Bytes(),
+	})
+	// Make sure there are no errors with sending the message
+	if err != nil {
+		jww.ERROR.Printf("PrecompDecrypt: Error received: %s", err)
+	}
+
+	return result, err
 }
 
 // Starts the local comm server
