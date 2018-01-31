92ddc10ab4a11b50ca8b2bf3f09fee48c1be83a9
Spencer Brown <spencer@privategrity.com>
2018-01-30 16:27:41 -0800

    Initial refactoring of AskOnline context setup to comms.

--- mixserver/mixserver.go
+++ mixserver/mixserver.go
@@ -43,6 +43,31 @@ func (s *server) AskOnline(ctx context.Context, err *pb.Ping) (
 	return &pb.Pong{}, nil
 }
 
+// Send an AskOnline message to a particular server
+func (s *server) SendAskOnline(addr string, message *pb.Ping) (*pb.Pong, error) {
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+
+	if err != nil {
+		jww.ERROR.Printf("Failed to connect to server with address %s",
+			addr)
+	}
+	// Removed a sleep here
+
+	c := pb.NewMixMessageServiceClient(conn)
+	// Send AskOnline Request and check that we get an AskOnlineAck back
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+	result, err = c.AskOnline(ctx, &pb.Ping{})
+	if err != nil {
+		jww.ERROR.Printf("AskOnline: Error received: %s", err)
+	} else {
+		jww.INFO.Printf("AskOnline: %v is online!", servers[i])
+	}
+	cancel()
+	conn.Close()
+
+	return result, nil
+}
+
 // Handle a PrecompDecrypt event
 func (s *server) PrecompDecrypt(ctx context.Context, err *pb.PrecompDecryptMessage) (
 	*pb.Ack, error) {
