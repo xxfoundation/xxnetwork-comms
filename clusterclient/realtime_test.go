666c0c39ed616cd301b3587bb8833c69c5244173
Richard T. Carback III <rick.carback@gmail.com>
2018-02-28 20:49:37 +0000

    Move all realtime comms messages to realtime.go

---
+++ clusterclient/realtime_test.go
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2018 Privategrity Corporation                                   /
+//                                                                             /
+// All rights reserved.                                                        /
+////////////////////////////////////////////////////////////////////////////////
+
+package clusterclient
+
+import (
+	pb "gitlab.com/privategrity/comms/mixmessages"
+	"testing"
+)
+
+// Smoke test SendRealtimePermute
+func TestSendRealtimePermute(t *testing.T) {
+	_, err := SendRealtimePermute(SERVER_ADDRESS, &pb.RealtimePermuteMessage{})
+	if err != nil {
+		t.Errorf("RealtimePermute: Error received: %s", err)
+	}
+}
+
+// Smoke test SendRealtimeEncrypt
+func TestSendRealtimeEncrypt(t *testing.T) {
+	_, err := SendRealtimeEncrypt(SERVER_ADDRESS, &pb.RealtimeEncryptMessage{})
+	if err != nil {
+		t.Errorf("RealtimeEncrypt: Error received: %s", err)
+	}
+}
+
+// Smoke test SendRealtimeDecrypt
+func TestSendRealtimeDecrypt(t *testing.T) {
+	_, err := SendRealtimeDecrypt(SERVER_ADDRESS, &pb.RealtimeDecryptMessage{})
+	if err != nil {
+		t.Errorf("RealtimeDecrypt: Error received: %s", err)
+	}
+}
