5d65f95f046bed009dd6105b6fa60d9b7798af1d
willcarter <will@privategrity.com>
2018-04-13 14:11:28 -0700

    added test file

---
+++ clusterclient/connectionmanager_test.go
@@ -0,0 +1,36 @@
+package clusterclient
+
+import (
+"testing"
+)
+
+// Function to test the Disconnect
+// Checks if conn established in Connect() is deleted.
+func TestDisconnect(t *testing.T) {
+
+	test := 2
+	pass := 0
+	address := SERVER_ADDRESS
+
+	Connect(address)
+
+	_, alive := connections[address]
+
+	if !alive {
+		t.Errorf("Connect Function did not working properly")
+	} else {
+		pass++
+	}
+
+	Disconnect(address)
+
+	_, present := connections[address]
+
+	if present {
+		t.Errorf("Disconnect Function not working properly")
+	} else {
+		pass++
+	}
+
+	println("Connection Manager Test: ", pass, "out of", test, "tests passed.")
+}
