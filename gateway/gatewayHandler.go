07add8fa9b10893068821e6afd9cac9b45526614
Richard T. Carback III <rick.carback@gmail.com>
2018-05-31 16:24:58 +0000

    GatewayHandler -> Handler
    Also fix the Id -> ID parameter names, add a comment for ReceiveBatch, and add messageID to the CheckMessages function

--- gateway/gatewayHandler.go
+++ gateway/gatewayHandler.go
@@ -10,13 +10,14 @@ import (
 	pb "gitlab.com/privategrity/comms/mixmessages"
 )
 
-type GatewayHandler interface {
+// Handler implementation for the Gateway
+type Handler interface {
 	// Return any MessageIDs in the buffer for this UserID
-	CheckMessages(userId uint64) ([]string, bool)
+	CheckMessages(userID uint64, messageID string) ([]string, bool)
 	// Returns the message matching the given parameters to the client
-	GetMessage(userId uint64, msgId string) (*pb.CmixMessage, bool)
+	GetMessage(userID uint64, msgID string) (*pb.CmixMessage, bool)
 	// Upload a message to the cMix Gateway
 	PutMessage(*pb.CmixMessage) bool
-	//
+	// ReceiveBatch receives message from a cMix node
 	ReceiveBatch(messages *pb.OutputMessages)
 }
