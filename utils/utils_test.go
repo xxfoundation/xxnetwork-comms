ad50741dc0be2122c694efac98544492c2c75def
jaketaylor <jake@privategrity.com>
2018-11-18 15:35:05 -0500

    Added test for utils

---
+++ utils/utils_test.go
@@ -0,0 +1,19 @@
+package utils
+
+import "testing"
+
+func TestGetFullPath_happy(t *testing.T) {
+	path := "~/test123"
+	newPath := GetFullPath(path)
+	if len(path) > len(newPath) {
+		t.Errorf("GetFullPath: Expected to replace ~!")
+	}
+}
+
+func TestGetFullPath_default(t *testing.T) {
+	path := "/test123/cat/dog"
+	newPath := GetFullPath(path)
+	if len(path) != len(newPath) {
+		t.Errorf("GetFullPath: Expected no replacement!")
+	}
+}
