272334a2310c8eb6e020c6b544227e1de5e11d14
Spencer Brown <spencer@privategrity.com>
2019-04-03 10:59:16 -0700

    Add test for TLS connection from server to server

--- node/node_test.go
+++ node/node_test.go
@@ -8,6 +8,9 @@ package node
 
 import (
 	"fmt"
+	"gitlab.com/elixxir/comms/connect"
+	"gitlab.com/elixxir/comms/mixmessages"
+	"gitlab.com/elixxir/comms/testkeys"
 	"math/rand"
 	"os"
 	"testing"
@@ -20,4 +23,16 @@ func TestMain(m *testing.M) {
 	rand.Seed(time.Now().Unix())
 	ServerAddress = fmt.Sprintf("localhost:%d", rand.Intn(2000)+4000)
 	os.Exit(m.Run())
+}
+
+// Tests whether the server can be connected to and run an RPC with TLS enabled
+func TestTLS(t *testing.T) {
+	connect.ServerCertPath = testkeys.GetNodeCertPath()
+	shutdown := StartServer(ServerAddress, NewImplementation(),
+		testkeys.GetNodeCertPath(), testkeys.GetNodeKeyPath())
+    defer shutdown()
+    _, err := SendAskOnline(ServerAddress, &mixmessages.Ping{})
+	if err != nil {
+		t.Error(err)
+	}
 }
---
+++ testkeys/keypath.go
@@ -0,0 +1,29 @@
+package testkeys
+
+import (
+    "path/filepath"
+    "runtime"
+)
+
+func getDirForFile() string {
+    // Get the filename we're in
+    _, currentFile, _, _ := runtime.Caller(0)
+    return filepath.Dir(currentFile)
+}
+
+// These functions are used to cover TLS connection code in tests
+func GetNodeCertPath() string {
+    return filepath.Join(getDirForFile(), "cmix.rip.crt")
+}
+
+func GetNodeKeyPath() string {
+    return filepath.Join(getDirForFile(), "cmix.rip.key")
+}
+
+func GetGatewayCertPath() string {
+    return filepath.Join(getDirForFile(), "gateway.cmix.rip.crt")
+}
+
+func GetGatewayKeyPath() string {
+    return filepath.Join(getDirForFile(), "gateway.cmix.rip.key")
+}
