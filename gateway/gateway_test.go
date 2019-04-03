c861d1e52fffa3a015c7f5cf3c721ef8d2cb0b65
Spencer Brown <spencer@privategrity.com>
2019-04-03 10:01:23 -0700

    Make gateway tests more reliable

--- gateway/gateway_test.go
+++ gateway/gateway_test.go
@@ -20,7 +20,11 @@ var ServerAddress = ""
 // This sets up a dummy/mock gateway instance for testing purposes
 func TestMain(m *testing.M) {
 	rand.Seed(time.Now().Unix())
-	GatewayAddress = fmt.Sprintf("localhost:%d", 6001)
-	ServerAddress = fmt.Sprintf("localhost:%d", 5001)
+	GatewayAddress = fmt.Sprintf("localhost:%d", rand.Intn(3000)+4000)
+	ServerAddress = fmt.Sprintf("localhost:%d", rand.Intn(3000)+3000)
+	// If they're the same address, keep trying until they're different
+	for ServerAddress == GatewayAddress {
+		ServerAddress = fmt.Sprintf("localhost:%d", rand.Intn(3000)+3000)
+	}
 	os.Exit(m.Run())
 }
