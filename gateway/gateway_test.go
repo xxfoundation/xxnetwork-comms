2bbb252ae941cce793ca1378cd0f13bfdfeadc85
jaketaylor <jake@privategrity.com>
2019-04-30 14:10:49 -0400

    Changed addressing for tests

--- gateway/gateway_test.go
+++ gateway/gateway_test.go
@@ -20,7 +20,7 @@ var ServerAddress = ""
 // This sets up a dummy/mock gateway instance for testing purposes
 func TestMain(m *testing.M) {
 	rand.Seed(time.Now().Unix())
-	GatewayAddress = fmt.Sprintf("localhost:%d", 6001)
-	ServerAddress = fmt.Sprintf("localhost:%d", 5001)
+	GatewayAddress = fmt.Sprintf("0.0.0.0:%d", 6001)
+	ServerAddress = fmt.Sprintf("0.0.0.0:%d", 5001)
 	os.Exit(m.Run())
 }
