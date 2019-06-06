f8c09f8c59c8e5ab35faf3e676ea488cb1796322
Tigran Avakyan <tigran.avakyan@icloud.com>
2019-06-06 16:33:13 -0700

    Fixed header in test

--- node/phase_test.go
+++ node/phase_test.go
@@ -7,10 +7,14 @@
 package node
 
 import (
+	"errors"
+	"github.com/golang/protobuf/proto"
 	"gitlab.com/elixxir/comms/connect"
 	"gitlab.com/elixxir/comms/mixmessages"
 	"gitlab.com/elixxir/comms/testkeys"
+	"google.golang.org/grpc/metadata"
 	"io"
+	"reflect"
 	"testing"
 )
 
@@ -48,20 +52,23 @@ func TestPhase_StreamPostPhase(t *testing.T) {
 	// Create streaming context so you can close stream later
 	ctx, cancel := connect.StreamingContext()
 
-	//// Create round info to be used in batch info
-	//roundInfo := mixmessages.RoundInfo{
-	//	ID: uint64(10),
-	//}
-	//
-	//// Create header which contains the batch info
-	//batchInfo := mixmessages.BatchInfo{
-	//	Round:    &roundInfo,
-	//	ForPhase: 3,
-	//}
-	//
-	//// Create a new context with some metadata
-	//ctx = metadata.AppendToOutgoingContext(ctx,
-	//	"BatchInfo", batchInfo.String())
+	// Create round info to be used in batch info
+	roundId := uint64(10)
+	roundInfo := mixmessages.RoundInfo{
+		ID: roundId,
+	}
+
+	// Create header which contains the batch info
+	forPhase := int32(3)
+	batchInfo := mixmessages.BatchInfo{
+		Round:    &roundInfo,
+		ForPhase: forPhase,
+	}
+
+	// Create a new context with some metadata
+	// using the batch info header
+	//headerBuffer, err := proto.Marshal(&batchInfo)
+	ctx = metadata.AppendToOutgoingContext(ctx, "batchinfo", batchInfo.String())
 
 	// Get stream client for post phase
 	streamClient, err := serverStreamSender.GetPostPhaseStream(senderToReceiverID, ctx)
@@ -70,9 +77,12 @@ func TestPhase_StreamPostPhase(t *testing.T) {
 		t.Errorf("Unable to get streamling clinet %v", err)
 	}
 
-	slots := createSlots(3)
+	// Generate indexed slots
+	numSlots := 3
+	slots := createSlots(numSlots)
 
 	// The server will send the slot messages and wait for ack
+	// and close on receiving it.
 	go func(slots []mixmessages.Slot) {
 		for i, slot := range slots {
 			err := streamClient.Send(&slot)
@@ -81,12 +91,13 @@ func TestPhase_StreamPostPhase(t *testing.T) {
 			}
 		}
 		ack, err := streamClient.CloseAndRecv()
+
 		if err != nil {
 			t.Errorf("Failed to close and receive stream")
 		}
 
 		if ack != nil && ack.Error != "" {
-			t.Errorf("Remote Server Error: %s", ack.Error)
+			t.Errorf("Remote Server Error in ack: %s", ack.Error)
 		}
 
 	}(slots)
@@ -94,26 +105,62 @@ func TestPhase_StreamPostPhase(t *testing.T) {
 	// Block until stream client context is cleaned up
 	<-streamClient.Context().Done()
 
+	// Compared received slots to expected values
+	for i, slot := range slots {
+		if !reflect.DeepEqual(*receivedBatch.Slots[i], slot) {
+			t.Errorf("Received slot %v doesn't match expected value: %v, got %v", i, slot, *receivedBatch.Slots[i])
+		}
+	}
+
+	// Compare for phase
+	if forPhase != receivedBatch.ForPhase {
+		t.Errorf("ForPhase received %v doesn't match expected %v", receivedBatch.ForPhase, forPhase)
+	}
+
+	// Compare round info
+	if !reflect.DeepEqual(roundInfo, *receivedBatch.Round) {
+		t.Errorf("Round info received %v doesn't match expected %v", *receivedBatch.Round, roundInfo)
+	}
+
 	// Clean up sender context
 	cancel()
 }
 
 var receivedBatch mixmessages.Batch
 
-func mockStreamPostPhase(server mixmessages.Node_StreamPostPhaseServer) error {
+func mockStreamPostPhase(stream mixmessages.Node_StreamPostPhaseServer) error {
+
+	// Unmarshal header into batch info
+	batchInfo := mixmessages.BatchInfo{}
+	md, ok := metadata.FromIncomingContext(stream.Context())
+
+	if !ok {
+		return errors.New("unable to retrieve meta data / header %v")
+	}
+
+	err := proto.UnmarshalText(md.Get("batchinfo")[0], &batchInfo)
+	if err != nil {
+		return err
+	}
+
+	// Create batch using batch info header
 	receivedBatch = mixmessages.Batch{
-		Slots: make([]*mixmessages.Slot, 3),
+		Round:    batchInfo.Round,
+		ForPhase: batchInfo.ForPhase,
+		Slots:    make([]*mixmessages.Slot, 3),
 	}
 
 	// Send a chunk per received slot until EOF
 	index := uint32(0)
 	for {
-		slot, err := server.Recv()
+		slot, err := stream.Recv()
 		// If we are at end of receiving
 		// send ack and finish
 		if err == io.EOF {
-			ack := mixmessages.Ack{}
-			err = server.SendAndClose(&ack)
+			ack := mixmessages.Ack{
+				Error: "",
+			}
+			err = stream.SendAndClose(&ack)
 			return err
 		}
 
