3fdeb20d3d7c6047f299f76193c07f8290a73204
jaketaylor <jake@privategrity.com>
2019-06-18 11:04:29 -0700

    Implemented NodeTopology

---
+++ registration/permissioning.go
@@ -0,0 +1,30 @@
+package registration
+
+import (
+	"errors"
+	"fmt"
+	jww "github.com/spf13/jwalterweatherman"
+	"gitlab.com/elixxir/comms/connect"
+	pb "gitlab.com/elixxir/comms/mixmessages"
+)
+
+// Send a message to the gateway
+func (r *RegistrationComms) SendNodeTopology(id fmt.Stringer,
+	message *pb.NodeTopology) error {
+
+	// Attempt to connect to addr
+	connection := r.GetNodeConnection(id)
+	ctx, cancel := connect.DefaultContext()
+
+	// Send the message
+	_, err := connection.DownloadTopology(ctx, message)
+
+	// Make sure there are no errors with sending the message
+	if err != nil {
+		err = errors.New(err.Error())
+		jww.ERROR.Printf("SendNodeToplogy: Error received: %+v", err)
+	}
+
+	cancel()
+	return err
+}
