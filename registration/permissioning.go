a9a10c31d554683801a66903dafbf80d1f90225f
jbhusson <jonah@elixxir.io>
2019-07-19 09:19:39 -0700

    Intitial commit adding any.proto (grpc generic type) and basic structure for SignedMessages (WIP)

--- registration/permissioning.go
+++ registration/permissioning.go
@@ -8,6 +8,7 @@ package registration
 import (
 	"errors"
 	"fmt"
+	"github.com/golang/protobuf/ptypes"
 	jww "github.com/spf13/jwalterweatherman"
 	"gitlab.com/elixxir/comms/connect"
 	pb "gitlab.com/elixxir/comms/mixmessages"
@@ -21,8 +22,22 @@ func (r *RegistrationComms) SendNodeTopology(id fmt.Stringer,
 	connection := r.GetNodeConnection(id)
 	ctx, cancel := connect.DefaultContext()
 
+	// Wrap message as a generic
+	anyMessage, err := ptypes.MarshalAny(message)
+	if err != nil {
+		jww.ERROR.Printf("ERROR OUT HERE: %+v", err)
+	}
+
+	// Get a signature
+
+	// Form signed message
+	signed := pb.SignedMessage{
+		Message:   anyMessage,
+		Signature: []byte("test for now"),
+	}
+
 	// Send the message
-	_, err := connection.DownloadTopology(ctx, message)
+	_, err = connection.DownloadTopology(ctx, message)
 
 	// Make sure there are no errors with sending the message
 	if err != nil {
