c1d92c58d7380312c52cf09946527420aa321bba
jbhusson <jonah@elixxir.io>
2019-08-06 13:11:21 -0400

    Add send function and test

--- gateway/registration_test.go
+++ gateway/registration_test.go
@@ -47,3 +47,20 @@ func TestSendConfirmNonceMessage(t *testing.T) {
 		t.Errorf("SendConfirmNonceMessage: Error received: %s", err)
 	}
 }
+
+func TestSendGetSignedCertMessage(t *testing.T) {
+	GatewayAddress := getNextGatewayAddress()
+	ServerAddress := getNextServerAddress()
+
+	gateway := StartGateway(GatewayAddress, NewImplementation(), nil, nil)
+	server := node.StartNode(ServerAddress, node.NewImplementation(), nil, nil)
+	defer gateway.Shutdown()
+	defer server.Shutdown()
+	connID := MockID("gatewayToServer")
+	gateway.ConnectToNode(connID, ServerAddress, nil)
+
+	_, err := gateway.SendGetSignedCertMessage(connID, &pb.Ping{})
+	if err != nil {
+		t.Errorf("SendGetSignedCertMessage: Error received: %s", err)
+	}
+}
