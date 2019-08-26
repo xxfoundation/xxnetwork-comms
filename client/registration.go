ec58ed2decc0ace2aad41a0d216328efe8be8c35
Niamh Nikali <niamh@elixxir.io>
2019-08-26 14:44:32 -0700

    Help the client call CheckClientVersion RPC

--- client/registration.go
+++ client/registration.go
@@ -35,3 +35,24 @@ func (c *ClientComms) SendRegistrationMessage(id fmt.Stringer,
 	cancel()
 	return response, err
 }
+
+// Call CheckClientVersion on the registration server
+func (c *ClientComms) SendCheckClientVersionMessage(id fmt.Stringer,
+	message *pb.ClientVersion) (*pb.ClientVersionConfirmation, error) {
+	// Get the connection
+	connection := c.GetRegistrationConnection(id)
+	ctx, cancel := connect.MessagingContext()
+
+	// Send the message
+	response, err := connection.CheckClientVersion(ctx, message)
+
+	// Log if we got an error
+	if err != nil {
+		err = errors.New(err.Error())
+		jww.ERROR.Printf("CheckClientVersion: Error received: %+v", err)
+	}
+
+	// Finish up
+	cancel()
+	return response, err
+}
