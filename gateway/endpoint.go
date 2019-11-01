336fbec23fa7f95b0490d55b32c8f37a52efda74
jaketaylor <jake@privategrity.com>
2019-11-01 14:04:56 -0700

    refactor for branch updates

--- gateway/endpoint.go
+++ gateway/endpoint.go
@@ -9,31 +9,24 @@
 package gateway
 
 import (
+	"gitlab.com/elixxir/comms/connect"
 	pb "gitlab.com/elixxir/comms/mixmessages"
 	"gitlab.com/elixxir/primitives/id"
 	"golang.org/x/net/context"
-	"google.golang.org/grpc/peer"
-	"net"
 )
 
 // Sends new MessageIDs in the buffer to a client
-func (g *GatewayComms) CheckMessages(ctx context.Context, msg *pb.ClientRequest) (
-	*pb.IDList, error) {
+func (g *GatewayComms) CheckMessages(ctx context.Context,
+	msg *pb.ClientRequest) (*pb.IDList, error) {
 
 	// Get peer information from context
-	p, ok := peer.FromContext(ctx)
-	if !ok {
-		return &pb.IDList{}, nil
-	}
-
-	// Strip port from IP address
-	ipAddress, _, err := net.SplitHostPort(p.Addr.String())
+	addr, _, err := connect.GetAddressFromContext(ctx)
 	if err != nil {
 		return nil, err
 	}
 
 	userID := id.NewUserFromBytes(msg.UserID)
-	msgIds, err := g.handler.CheckMessages(userID, msg.LastMessageID, ipAddress)
+	msgIds, err := g.handler.CheckMessages(userID, msg.LastMessageID, addr)
 	returnMsg := &pb.IDList{}
 	if err == nil {
 		returnMsg.IDs = msgIds
@@ -44,20 +37,15 @@ func (g *GatewayComms) CheckMessages(ctx context.Context, msg *pb.ClientRequest)
 // Sends a message matching the given parameters to a client
 func (g *GatewayComms) GetMessage(ctx context.Context, msg *pb.ClientRequest) (
 	*pb.Slot, error) {
-	// Get peer information from context
-	p, ok := peer.FromContext(ctx)
-	if !ok {
-		return &pb.Slot{}, nil
-	}
 
-	// Strip port from IP address
-	ipAddress, _, err := net.SplitHostPort(p.Addr.String())
+	// Get peer information from context
+	addr, _, err := connect.GetAddressFromContext(ctx)
 	if err != nil {
 		return nil, err
 	}
 
 	userID := id.NewUserFromBytes(msg.UserID)
-	returnMsg, err := g.handler.GetMessage(userID, msg.LastMessageID, ipAddress)
+	returnMsg, err := g.handler.GetMessage(userID, msg.LastMessageID, addr)
 	if err != nil {
 		// Return an empty message if no results
 		returnMsg = &pb.Slot{}
@@ -70,19 +58,13 @@ func (g *GatewayComms) PutMessage(ctx context.Context, msg *pb.Slot) (*pb.Ack,
 	error) {
 
 	// Get peer information from context
-	p, ok := peer.FromContext(ctx)
-	if !ok {
-		return &pb.Ack{}, nil
-	}
-
-	// Strip port from IP address
-	ipAddress, _, err := net.SplitHostPort(p.Addr.String())
+	addr, _, err := connect.GetAddressFromContext(ctx)
 	if err != nil {
 		return nil, err
 	}
 
 	// Upload a message to the cMix Gateway at the peer's IP address
-	err = g.handler.PutMessage(msg, ipAddress)
+	err = g.handler.PutMessage(msg, addr)
 
 	return &pb.Ack{}, err
 }
@@ -92,18 +74,12 @@ func (g *GatewayComms) RequestNonce(ctx context.Context,
 	msg *pb.NonceRequest) (*pb.Nonce, error) {
 
 	// Get peer information from context
-	p, ok := peer.FromContext(ctx)
-	if !ok {
-		return &pb.Nonce{}, nil
-	}
-
-	// Strip port from IP address
-	ipAddress, _, err := net.SplitHostPort(p.Addr.String())
+	addr, _, err := connect.GetAddressFromContext(ctx)
 	if err != nil {
 		return nil, err
 	}
 
-	return g.handler.RequestNonce(msg, ipAddress)
+	return g.handler.RequestNonce(msg, addr)
 }
 
 // Pass-through for Registration Nonce Confirmation
@@ -112,16 +88,10 @@ func (g *GatewayComms) ConfirmNonce(ctx context.Context,
 	error) {
 
 	// Get peer information from context
-	p, ok := peer.FromContext(ctx)
-	if !ok {
-		return &pb.RegistrationConfirmation{}, nil
-	}
-
-	// Strip port from IP address
-	ipAddress, _, err := net.SplitHostPort(p.Addr.String())
+	addr, _, err := connect.GetAddressFromContext(ctx)
 	if err != nil {
 		return nil, err
 	}
 
-	return g.handler.ConfirmNonce(msg, ipAddress)
+	return g.handler.ConfirmNonce(msg, addr)
 }
