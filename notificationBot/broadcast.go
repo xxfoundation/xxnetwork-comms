37708c661653e4680669f907c17962ed7430c1c9
josh <josh@elixxir.io>
2020-01-22 13:11:20 -0800

    Unify requestNDF logic into Protocomms
    Also repond to MR comments

--- notificationBot/broadcast.go
+++ notificationBot/broadcast.go
@@ -6,48 +6,15 @@
 package notificationBot
 
 import (
-	"github.com/golang/protobuf/ptypes"
-	"github.com/golang/protobuf/ptypes/any"
-	"github.com/pkg/errors"
-	jww "github.com/spf13/jwalterweatherman"
 	"gitlab.com/elixxir/comms/connect"
 	pb "gitlab.com/elixxir/comms/mixmessages"
-	"google.golang.org/grpc"
 )
 
 // NotificationBot -> Permissioning
-func (nb *Comms) RequestNdf(host *connect.Host,
-	message *pb.NDFHash) (*pb.NDF, error) {
+func (nb *Comms) RequestNdf(host *connect.Host, message *pb.NDFHash) (*pb.NDF, error) {
 
-	// Create the Send Function
-	f := func(conn *grpc.ClientConn) (*any.Any, error) {
-		// Set up the context
-		ctx, cancel := connect.MessagingContext()
-		defer cancel()
-
-		authMsg, err := nb.PackAuthenticatedMessage(message, host, false)
-		if err != nil {
-			return nil, errors.New(err.Error())
-		}
-		// Send the message
-		resultMsg, err := pb.NewRegistrationClient(
-			conn).PollNdf(ctx, authMsg)
-		if err != nil {
-			return nil, errors.New(err.Error())
-		}
-		return ptypes.MarshalAny(resultMsg)
-	}
-
-	// Execute the Send function
-	jww.DEBUG.Printf("Sending Request Ndf message: %+v", message)
-	resultMsg, err := nb.Send(host, f)
-	if err != nil {
-		return nil, err
-	}
-
-	// Marshall the result
-	result := &pb.NDF{}
-	return result, ptypes.UnmarshalAny(resultMsg, result)
+	// Call the ProtoComms RequestNdf call
+	return nb.ProtoComms.RequestNdf(host, message)
 }
 
 /* TODO: Uncomment when XX-1777 is done (Add PollForNotifications to Gateways)
