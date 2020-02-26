3b00a9ed0b4bb86ad0d48f658ce8c887da1bb5df
jbhusson <jonah@elixxir.io>
2020-02-26 14:07:12 -0800

    finish basic implementation of ringbuff

--- consensus/datastructure.go
+++ consensus/datastructure.go
@@ -9,56 +9,71 @@ import (
 const RoundInfoBufLen = 1000
 const RoundUpdatesBufLen = 10000
 
+type idFunc func(interface{}) uint64
+type compFunc func(interface{}, interface{}) bool
+
 // Standard ring buffer, but objects come with numbering
 type Updates struct {
 	updates *RingBuff
 }
 
-func (u *Updates) AddRound(info *mixmessages.RoundInfo) {
-	if u.updates == nil {
-		u.updates = New(RoundUpdatesBufLen)
+func (u *Updates) AddRound(info *mixmessages.RoundInfo) error {
+	// comparison should ensure that updates are not overwritten in the event of a duplicate
+	comp := func(current interface{}, new interface{}) bool {
+		if current == nil {
+			return true
+		}
+		return false
 	}
 
-	nextId := u.updates.GetMostRecent().(*mixmessages.RoundInfo).UpdateID + 1
-	if nextId == info.UpdateID {
-		u.updates.Push(info)
-	} else if nextId < info.UpdateID {
-		for i := nextId; i < info.UpdateID; i++ {
-			u.updates.Push(nil)
-		}
-		u.updates.Push(info)
-	} else if nextId > info.ID {
-		i := int(info.UpdateID - nextId)
-		cur := u.updates.Get(i)
-		if cur != nil {
-			u.updates.PushToIndex(info, i)
-		} else {
-			// what happens when rounds collide
-		}
+	// we want each updateId stored in this structure
+	id := func(val interface{}) uint64 {
+		return val.(*mixmessages.RoundInfo).UpdateID
 	}
+
+	if u.updates == nil {
+		u.updates = New(RoundUpdatesBufLen, id)
+	}
+
+	return u.updates.UpsertById(info, id, comp)
 }
 
 // ID numbers can overwrite
 type Data struct {
-	rounds        [RoundInfoBufLen]*mixmessages.RoundInfo
-	earliestRound uint64
-	letestRound   uint64
+	rounds *RingBuff
 }
 
 func (d *Data) UpsertRound(r *mixmessages.RoundInfo) error {
-	if d.earliestRound > r.ID {
-		return errors.New("update to untracked round")
+	// comparison here should ensure that either the current round is nil or has a lower update id than the new round
+	comp := func(current interface{}, new interface{}) bool {
+		if current == nil {
+			return true
+		}
+		if current.(*mixmessages.RoundInfo).UpdateID < new.(*mixmessages.RoundInfo).UpdateID {
+			return true
+		}
+		return false
+	}
+
+	// We want data using the round ID as its primary
+	id := func(val interface{}) uint64 {
+		return val.(*mixmessages.RoundInfo).ID
+	}
+
+	if d.rounds == nil {
+		d.rounds = New(RoundInfoBufLen, id)
 	}
 
 	//find the round location
 	//check the new state is newer then the current
 	//replace the round info object
-	return nil
+	return d.rounds.UpsertById(r, id, comp)
 }
 
 type RingBuff struct {
 	buff             []interface{}
 	len, first, last int
+	id               idFunc
 	lock             sync.Mutex
 }
 
@@ -87,42 +102,73 @@ func (rb *RingBuff) getIndex(i int) int {
 }
 
 // Initialize a new ring buffer with length n
-func New(n int) *RingBuff {
+func New(n int, id idFunc) *RingBuff {
 	rb := &RingBuff{
 		buff:  make([]interface{}, 0),
 		len:   n,
 		first: -1,
 		last:  0,
+		id:    id,
 	}
 	return rb
 }
 
 // Push a round to the buffer
-func (rb *RingBuff) Push(r *mixmessages.RoundInfo) {
+func (rb *RingBuff) Push(val interface{}) {
 	rb.lock.Lock()
 	defer rb.lock.Unlock()
 
-	rb.buff[rb.last] = r
+	rb.buff[rb.last] = val
 	rb.next()
 }
 
 // push a round to a relative index in the buffer
-func (rb *RingBuff) PushToIndex(r *mixmessages.RoundInfo, i int) {
+func (rb *RingBuff) UpsertById(val interface{}, id idFunc, comp compFunc) error {
 	rb.lock.Lock()
 	defer rb.lock.Unlock()
+	newId := id(val)
 
-	index := rb.getIndex(i)
-	rb.buff[index] = r
+	if id(rb.buff[rb.first]) > newId {
+		return errors.Errorf("Did not upsert value %+v; id is older than first tracked", val)
+	}
+
+	lastId := id(rb.Get())
+	if lastId+1 == newId {
+		rb.Push(val)
+	} else if (lastId + 1) < newId {
+		for i := lastId + 1; i <= newId; i++ {
+			rb.Push(nil)
+		}
+		rb.Push(val)
+	} else if lastId+1 > newId {
+		i := rb.getIndex(int(newId - (lastId + 1)))
+		if comp(rb.buff[i], val) {
+			rb.buff[i] = val
+		} else {
+			return errors.Errorf("Did not upsert value %+v; comp function returned false", val)
+		}
+	}
+	return nil
 }
 
-func (rb *RingBuff) GetMostRecent() interface{} {
+func (rb *RingBuff) Get() interface{} {
 	mostRecentIndex := (rb.last + rb.len - 1) % rb.len
 	return rb.buff[mostRecentIndex]
 }
 
-func (rb *RingBuff) Get(i int) interface{} {
-	index := rb.getIndex(i)
-	return rb.buff[index]
+func (rb *RingBuff) GetById(i int) (interface{}, error) {
+	firstId := rb.id(rb.buff[rb.first])
+	if i < int(firstId) {
+		return nil, errors.Errorf("requested ID %d is lower than oldest id %d", i, firstId)
+	}
+
+	lastId := rb.id(rb.Get())
+	if i > int(lastId) {
+		return nil, errors.Errorf("requested id %d is higher than most recent id %d", i, lastId)
+	}
+
+	index := rb.getIndex(int(firstId) - i)
+	return rb.buff[index], nil
 }
 
 func (rb *RingBuff) Len() int {
