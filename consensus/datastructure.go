0eeb70f3c9892af6f3dbe8eb4b743849938c644a
jbhusson <jonah@elixxir.io>
2020-02-26 16:44:21 -0800

    add comments

--- consensus/datastructure.go
+++ consensus/datastructure.go
@@ -1,6 +1,7 @@
 package consensus
 
 import (
+	"github.com/pkg/errors"
 	"gitlab.com/elixxir/comms/mixmessages"
 	ds "gitlab.com/elixxir/primitives/dataStructures"
 )
@@ -13,6 +14,7 @@ type Updates struct {
 	updates *ds.RingBuff
 }
 
+// Add a round to the ring buffer
 func (u *Updates) AddRound(info *mixmessages.RoundInfo) error {
 	// comparison should ensure that updates are not overwritten in the event of a duplicate
 	comp := func(current interface{}, new interface{}) bool {
@@ -24,6 +26,9 @@ func (u *Updates) AddRound(info *mixmessages.RoundInfo) error {
 
 	// we want each updateId stored in this structure
 	id := func(val interface{}) int {
+		if val == nil {
+			return -1
+		}
 		return int(val.(*mixmessages.RoundInfo).UpdateID)
 	}
 
@@ -34,11 +39,21 @@ func (u *Updates) AddRound(info *mixmessages.RoundInfo) error {
 	return u.updates.UpsertById(info, comp)
 }
 
+// Get a given update ID from the ring buffer
+func (u *Updates) GetUpdate(id int) (*mixmessages.RoundInfo, error) {
+	val, err := u.updates.GetById(id)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Failed to get update with id %d", id)
+	}
+	return val.(*mixmessages.RoundInfo), nil
+}
+
 // ID numbers can overwrite
 type Data struct {
 	rounds *ds.RingBuff
 }
 
+// Upsert a round into the ring bugger
 func (d *Data) UpsertRound(r *mixmessages.RoundInfo) error {
 	// comparison here should ensure that either the current round is nil or has a lower update id than the new round
 	comp := func(current interface{}, new interface{}) bool {
@@ -53,6 +68,9 @@ func (d *Data) UpsertRound(r *mixmessages.RoundInfo) error {
 
 	// We want data using the round ID as its primary
 	id := func(val interface{}) int {
+		if val == nil {
+			return -1
+		}
 		return int(val.(*mixmessages.RoundInfo).ID)
 	}
 
@@ -64,4 +82,13 @@ func (d *Data) UpsertRound(r *mixmessages.RoundInfo) error {
 	//check the new state is newer then the current
 	//replace the round info object
 	return d.rounds.UpsertById(r, comp)
+}
+
+// Get a given round id from the ring buffer
+func (d *Data) GetRound(id int) (*mixmessages.RoundInfo, error) {
+	val, err := d.rounds.GetById(id)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Failed to get update with id %d", id)
+	}
+	return val.(*mixmessages.RoundInfo), nil
 }
