03d16a6b2c455623be89b0be6311b7416a966e74
Benjamin Wenger <ben@elixxir.ioo>
2020-06-30 07:58:40 -0700

    added ip override to the network instance

---
+++ network/dataStructures/ipOverride_test.go
@@ -0,0 +1,79 @@
+package dataStructures
+
+import (
+	"gitlab.com/elixxir/primitives/id"
+	"testing"
+)
+
+//tests that NewIpOverrideList returns a properly formatted override list
+func TestNewIpOverrideList(t *testing.T) {
+	nol := NewIpOverrideList()
+
+	if nol.ipOverride == nil {
+		t.Errorf("Ip Override List should have a map")
+	}
+}
+
+// tests that creating an override works
+func TestIpOverrideList_Override(t *testing.T) {
+	iol := &IpOverrideList{
+		ipOverride: make(map[id.ID]string),
+	}
+
+	testID := id.NewIdFromUInt(42, id.Node, t)
+	testIP := "woop"
+
+	iol.Override(testID, testIP)
+
+	if len(iol.ipOverride) != 1 {
+		t.Errorf("IP override has the wrong length")
+	}
+
+	resultIP, exist := iol.ipOverride[*testID]
+
+	if !exist {
+		t.Errorf("could not find override in the map")
+	}
+
+	if resultIP != testIP {
+		t.Errorf("the ip returned from the map is not as expected"+
+			"expected: %s, recieved: %s", testIP, resultIP)
+	}
+}
+
+// tests that the old IP is passed through when there is no override
+func TestIpOverrideList_CheckOverride_NoOverride(t *testing.T) {
+	iol := &IpOverrideList{
+		ipOverride: make(map[id.ID]string),
+	}
+
+	testID := id.NewIdFromUInt(42, id.Node, t)
+	testIP := "woop"
+
+	dummyID := id.NewIdFromUInt(69, id.Node, t)
+	iol.Override(dummyID, "blarg")
+
+	resultIP := iol.CheckOverride(testID, testIP)
+	if resultIP != testIP {
+		t.Errorf("The returned ip is not as expected; "+
+			"Expected: %s, Returned: %s", testIP, resultIP)
+	}
+}
+
+// tests that the old IP overwritten through when there is an override
+func TestIpOverrideList_CheckOverride_ValidOverride(t *testing.T) {
+	iol := &IpOverrideList{
+		ipOverride: make(map[id.ID]string),
+	}
+
+	testID := id.NewIdFromUInt(42, id.Node, t)
+	testIP := "woop"
+
+	iol.Override(testID, testIP)
+
+	resultIP := iol.CheckOverride(testID, "blarg")
+	if resultIP != testIP {
+		t.Errorf("The returned ip is not as expected; "+
+			"Expected: %s, Returned: %s", testIP, resultIP)
+	}
+}
