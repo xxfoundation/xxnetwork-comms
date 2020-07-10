9b3787272e2d0799c6d8e7a18d8719bf541a849a
josh <josh@elixxir.io>
2020-07-10 09:54:28 -0700

    Port RetrieveNDF tests to client

--- client/registration_test.go
+++ client/registration_test.go
@@ -10,7 +10,9 @@ package client
 import (
 	pb "gitlab.com/elixxir/comms/mixmessages"
 	"gitlab.com/elixxir/comms/registration"
+	"gitlab.com/elixxir/comms/testutils"
 	"gitlab.com/elixxir/primitives/id"
+	"gitlab.com/elixxir/primitives/ndf"
 	"gitlab.com/xx_network/comms/connect"
 	"testing"
 )
@@ -91,5 +93,89 @@ func TestSendGetUpdatedNDF(t *testing.T) {
 
 	if err != nil {
 		t.Errorf("RequestNdf: Error received: %s", err)
+	}
+}
+
+// Test that Poll NDF handles all comms errors returned properly, and that it decodes and successfully returns an ndf
+func TestProtoComms_PollNdf(t *testing.T) {
+
+	// Define a client object
+	clientId := id.NewIdFromString("client", id.Generic, t)
+	c, err := NewClientComms(clientId, nil, nil, nil)
+	if err != nil {
+		t.Errorf("Can't create client comms: %+v", err)
+	}
+
+	mockPermServer := registration.StartRegistrationServer(&id.Permissioning, RegistrationAddr, RegistrationHandler, nil, nil)
+	defer mockPermServer.Shutdown()
+
+	newNdf := &ndf.NetworkDefinition{}
+
+	// Test that poll ndf fails if getHost returns an error
+	GetHostErrBool = false
+	RequestNdfErr = nil
+
+	_, err = c.RetrieveNdf(newNdf)
+
+	if err == nil {
+		t.Errorf("GetHost should have failed but it didnt't: %+v", err)
+		t.Fail()
+	}
+
+	// Test that pollNdf returns an error in this case
+	// This enters an infinite loop is there a way to fix this test?
+
+	// Test that pollNdf Fails if it cant decode the request msg
+	RequestNdfErr = nil
+	GetHostErrBool = true
+	NdfToreturn.Ndf = []byte(ExampleBadNdfJSON)
+	_, err = c.RetrieveNdf(newNdf)
+
+	if err == nil {
+		t.Logf("RequestNdf should have failed to parse bad ndf: %+v", err)
+		t.Fail()
+	}
+	_, err = c.ProtoComms.AddHost(&id.Permissioning, RegistrationAddr, nil, false, false)
+	if err != nil {
+		t.Errorf("Failed to add permissioning as a host: %+v", err)
+	}
+
+	// Test that pollNDf Is successful with expected result
+	RequestNdfErr = nil
+	GetHostErrBool = true
+	NdfToreturn.Ndf = []byte(testutils.ExampleJSON)
+	_, err = c.RetrieveNdf(newNdf)
+	//comms.mockManager.AddHost()
+	if err != nil {
+		t.Logf("Ndf failed to parse: %+v", err)
+		t.Fail()
+	}
+
+}
+
+// Happy path
+func TestProtoComms_PollNdfRepeatedly(t *testing.T) {
+	// Define a client object
+	clientId := id.NewIdFromString("client", id.Generic, t)
+	c, err := NewClientComms(clientId, nil, nil, nil)
+	if err != nil {
+		t.Errorf("Can't create client comms: %+v", err)
+	}
+	// Start up the mock reg server
+	mockPermServer := registration.StartRegistrationServer(&id.Permissioning, RegistrationAddrErr, RegistrationError, nil, nil)
+	defer mockPermServer.Shutdown()
+
+	// Add the host to the comms object
+	_, err = c.ProtoComms.AddHost(&id.Permissioning, RegistrationAddrErr, nil, false, false)
+	if err != nil {
+		t.Errorf("Failed to add permissioning as a host: %+v", err)
+	}
+
+	newNdf := &ndf.NetworkDefinition{}
+
+	// This should hit the loop until the number of retries is satisfied in the error handler
+	_, err = c.RetrieveNdf(newNdf)
+	if err != nil {
+		t.Errorf("Expected error case, should not return non-error until attempt #5")
 	}
 }
