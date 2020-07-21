174789d9ca295fcef6a3b9c6f66cc92b03d02055
jbhusson <jonah@elixxir.io>
2020-07-21 14:58:10 -0400

    Initial commit for gossip protocol

---
+++ gossip/endpoint.go
@@ -0,0 +1,24 @@
+package gossip
+
+import (
+	"context"
+	"gitlab.com/xx_network/comms/connect"
+	"gitlab.com/xx_network/comms/messages"
+)
+
+// Object used to embed Gossip functionality in higher-level Comms objects
+type Comms struct {
+	*connect.ProtoComms
+	*Manager
+}
+
+// Generic endpoint for forwarding GossipMsg to correct Protocol
+func (g *Comms) Endpoint(ctx context.Context, msg *messages.GossipMsg) (*messages.Ack, error) {
+	return &messages.Ack{}, g.Get(msg.Tag).receive(msg)
+}
+
+// Generic streaming endpoint for forwarding GossipMsg to correct Protocol
+func (g *Comms) Stream(ctx context.Context, msg *messages.GossipMsg) (*messages.Ack, error) {
+	// TODO: Will be implemented later on
+	return &messages.Ack{}, nil
+}
---
+++ gossip/protocol.go
@@ -0,0 +1,61 @@
+package gossip
+
+import (
+	"gitlab.com/elixxir/primitives/id"
+	"gitlab.com/xx_network/comms/connect"
+	"gitlab.com/xx_network/comms/messages"
+)
+
+// Defines the type of Gossip message fingerprints
+// hash(tag, origin, payload, signature)
+type Fingerprint [32]byte
+
+// Gossip-related configuration flag
+type Flags struct {
+	FanOut uint8
+}
+
+// Generic interface representing various Gossip protocols
+type Protocol struct {
+	comms *connect.ProtoComms
+
+	// Thread-safe record of all Gossip messages for this Protocol
+	// NOTE: Must avoid unlimited growth
+	fingerprints map[Fingerprint]struct{}
+
+	// Thread-safe list of peers for the Protocol
+	peers []*id.ID
+
+	// Stores the Gossip-related configuration flags
+	flags Flags
+
+	// Handler function for GossipMsg Reception
+	receiver Receiver
+
+	// Verifier function for GossipMsg signatures
+	verify SignatureVerification
+
+	// Marks a Protocol as Defunct such that it will ignore new messages
+	IsDefunct bool
+}
+
+// Receive a Gossip Message and check fingerprints map
+// (if unique calls GossipSignatureVerify -> Receiver)
+func (p *Protocol) receive(msg *messages.GossipMsg) error {
+	return nil
+}
+
+// Adds a peer by ID to the Gossip protocol
+func (p *Protocol) AddGossipPeer(id *id.ID) error {
+	return nil
+}
+
+// Builds and sends a GossipMsg
+func (p *Protocol) Gossip(msg *messages.GossipMsg) error {
+	return nil
+}
+
+// Performs returns which peers to send the GossipMsg to
+func (p *Protocol) getPeers() ([]*id.ID, error) {
+	return nil, nil
+}
