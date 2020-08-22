d96ae8100b139b083a873fc83651b766aaed516a
Niamh Nikali <niamh@elixxir.io>
2020-08-22 12:09:54 -0700

    Add tests

--- network/dataStructures/roundEvents.go
+++ network/dataStructures/roundEvents.go
@@ -20,7 +20,7 @@ import (
 type RoundEventCallback func(ri *pb.RoundInfo, timedOut bool)
 
 // One callback and associated data
-type eventCallback struct {
+type EventCallback struct {
 	// Round states where this function can be called
 	states []states.Round
 	// Send on this channel to cause the relevant callbacks
@@ -30,18 +30,24 @@ type eventCallback struct {
 // Holds the callbacks for a round
 type RoundEvents struct {
 	// The slice that map[id.Round] maps to is a collection of event callbacks for each of the round's states
-	callbacks map[id.Round][states.NUM_STATES]map[*eventCallback]*eventCallback
+	callbacks map[id.Round][states.NUM_STATES]map[*EventCallback]*EventCallback
 	mux       sync.RWMutex
 }
 
-func (r *RoundEvents) Remove(rid id.Round, e *eventCallback) {
+func NewRoundEvents() *RoundEvents {
+	return &RoundEvents{
+		callbacks: make(map[id.Round][states.NUM_STATES]map[*EventCallback]*EventCallback),
+	}
+}
+
+func (r *RoundEvents) Remove(rid id.Round, e *EventCallback) {
 	r.mux.Lock()
 	r.remove(rid, e)
 	r.mux.Unlock()
 }
 
 // Remove an event callback from all the states' maps
-func (r *RoundEvents) remove(rid id.Round, e *eventCallback) {
+func (r *RoundEvents) remove(rid id.Round, e *EventCallback) {
 	for _, s := range e.states {
 		delete(r.callbacks[rid][s], e)
 	}
@@ -57,7 +63,7 @@ func (r *RoundEvents) remove(rid id.Round, e *eventCallback) {
 	}
 }
 
-func (r *RoundEvents) signal(rid id.Round, event *eventCallback, callback RoundEventCallback, timeout time.Duration) {
+func (r *RoundEvents) signal(rid id.Round, event *EventCallback, callback RoundEventCallback, timeout time.Duration) {
 	ri := &pb.RoundInfo{ID: uint64(rid)}
 	select {
 	case <-time.After(timeout):
@@ -68,9 +74,10 @@ func (r *RoundEvents) signal(rid id.Round, event *eventCallback, callback RoundE
 	}
 }
 
-func (r *RoundEvents) AddRoundEvent(rid id.Round, callback RoundEventCallback, timeout time.Duration, validStates ...states.Round) {
+// Adds an event to the RoundEvents struct and returns its handle for possible deletion
+func (r *RoundEvents) AddRoundEvent(rid id.Round, callback RoundEventCallback, timeout time.Duration, validStates ...states.Round) *EventCallback {
 	// Add the specific event to the round
-	thisEvent := &eventCallback{
+	thisEvent := &EventCallback{
 		states: validStates,
 		signal: make(chan *pb.RoundInfo, 1),
 	}
@@ -82,7 +89,7 @@ func (r *RoundEvents) AddRoundEvent(rid id.Round, callback RoundEventCallback, t
 	if !ok {
 		// create callbacks for this round
 		for i := range callbacks {
-			callbacks[i] = make(map[*eventCallback]*eventCallback)
+			callbacks[i] = make(map[*EventCallback]*EventCallback)
 		}
 
 		r.callbacks[rid] = callbacks
@@ -92,6 +99,7 @@ func (r *RoundEvents) AddRoundEvent(rid id.Round, callback RoundEventCallback, t
 		callbacks[s][thisEvent] = thisEvent
 	}
 	r.mux.Unlock()
+	return thisEvent
 }
 
 func (r *RoundEvents) TriggerRoundEvent(ri *pb.RoundInfo) {
@@ -105,7 +113,7 @@ func (r *RoundEvents) TriggerRoundEvent(ri *pb.RoundInfo) {
 	thisStatesCallbacks := callbacks[ri.State]
 	if len(thisStatesCallbacks) != 0 {
 		// Keep track of events we've used for later removal
-		var events []*eventCallback
+		var events []*EventCallback
 		for _, event := range thisStatesCallbacks {
 			event.signal <- ri
 			events = append(events, event)
--- network/dataStructures/roundEvents_test.go
+++ network/dataStructures/roundEvents_test.go
@@ -1,15 +1,113 @@
 package dataStructures
 
-import "testing"
+import (
+	pb "gitlab.com/elixxir/comms/mixmessages"
+	"gitlab.com/elixxir/primitives/id"
+	"gitlab.com/elixxir/primitives/states"
+	"testing"
+	"time"
+)
 
+// AddRoundEvent should increase the number of round events in the data structure
 func TestRoundEvents_AddRoundEvent(t *testing.T) {
+	events := NewRoundEvents()
 
+	// Adding with no states should result in no states with callbacks
+	events.AddRoundEvent(id.Round(1), func(ri *pb.RoundInfo, timedOut bool) {}, time.Minute)
+	for _, callback := range events.callbacks[id.Round(1)] {
+		if len(callback) != 0 {
+			t.Error("Adding round event with no states shouldn't add callback")
+		}
+	}
+
+	// Adding with some states should result in one round added
+	events.AddRoundEvent(id.Round(1), func(ri *pb.RoundInfo, timedOut bool) {}, time.Minute, states.PENDING, states.QUEUED)
+	if len(events.callbacks) != 1 {
+		t.Error("Adding round event with some states should make 1 round in the map")
+	}
+
+	// Adding another with a same state should result in two events for that state
+	events.AddRoundEvent(id.Round(1), func(ri *pb.RoundInfo, timedOut bool) {}, time.Minute, states.PENDING)
+	if len(events.callbacks) != 1 {
+		t.Error("Adding round event with some states should make 1 round in the map")
+	}
+	if len(events.callbacks[id.Round(1)][states.PENDING]) != 2 {
+		t.Error("Pending should have two events")
+	}
+	if len(events.callbacks[id.Round(1)][states.QUEUED]) != 1 {
+		t.Error("Queued should have one event")
+	}
+
+	// It should be possible to add events to more than one round, of course
+	events.AddRoundEvent(id.Round(2), func(ri *pb.RoundInfo, timedOut bool) {}, time.Minute, states.PENDING)
+	if len(events.callbacks) != 2 {
+		t.Error("Should have 2 rounds' events in the map")
+	}
 }
 
+// AddRoundEvent should result in round timeouts after the specified amount of time
+func TestRoundEvents_AddRoundEvent_Timeout(t *testing.T) {
+	events := NewRoundEvents()
+
+	called := false
+	timeout := time.Millisecond
+	events.AddRoundEvent(id.Round(1), func(ri *pb.RoundInfo, timedOut bool) {
+		called = true
+		if !timedOut {
+			t.Error("Should have called event with timedOut true")
+		}
+	}, timeout, states.PENDING)
+
+	time.Sleep(timeout * 2)
+	if !called {
+		t.Error("Event callback should have been called to let us know that we timed out")
+	}
+}
+
+// Remove should remove one event from the data structure
+// If there was one add call, removing it should leave the map empty
 func TestRoundEvents_Remove(t *testing.T) {
-
+	events := NewRoundEvents()
+	rid := id.Round(1)
+	callback := events.AddRoundEvent(rid, func(ri *pb.RoundInfo, timedOut bool) {}, time.Minute, states.PENDING, states.QUEUED)
+	events.Remove(rid, callback)
+	if len(events.callbacks) != 0 {
+		t.Error("callbacks map should be empty after removing")
+	}
 }
 
+// Round events should be callable after being added
 func TestRoundEvents_TriggerRoundEvent(t *testing.T) {
+	// Normal path
+	events := NewRoundEvents()
+	rid := id.Round(1)
+	called := false
+	events.AddRoundEvent(rid, func(ri *pb.RoundInfo, timedOut bool) {
+		called = true
+	}, time.Minute, states.PENDING)
+	events.TriggerRoundEvent(&pb.RoundInfo{
+		ID:    uint64(rid),
+		State: uint32(states.PENDING),
+	})
 
+	// wait for calling
+	time.Sleep(5 * time.Millisecond)
+	if !called {
+		t.Error("callback should have been called")
+	}
+	if len(events.callbacks) != 0 {
+		t.Error("callback should have been removed after calling")
+	}
+
+	// No matching round events: nothing should happen
+	// (just to cover that branch)
+	called = false
+	events.TriggerRoundEvent(&pb.RoundInfo{
+		ID:    uint64(rid),
+		State: uint32(states.PENDING),
+	})
+	time.Sleep(5 * time.Millisecond)
+	if called {
+		t.Error("second trigger shouldn't have resulted in a call")
+	}
 }
