2550122966bf926b4dacbb3ebf31b324c94b72e8
josh <josh@elixxir.io>
2020-08-31 14:05:26 -0700

    Add GossipBatch function to gateway

--- gateway/gossip.go
+++ gateway/gossip.go
@@ -7,7 +7,10 @@
 package gateway
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/pkg/errors"
+	pb "gitlab.com/elixxir/comms/mixmessages"
 	"gitlab.com/xx_network/comms/gossip"
 	"gitlab.com/xx_network/primitives/id"
 )
@@ -15,7 +18,7 @@ import (
 // Sets up a new gossip manager connected to the gateway's comms object.
 // It implicitly initializes a gossip for rate limiting purposes
 func (g *Comms) SetUpGossip(gossipFlags gossip.ManagerFlags, receiver gossip.Receiver,
-	verifier gossip.SignatureVerification,	peers []*id.ID)  {
+	verifier gossip.SignatureVerification, peers []*id.ID) {
 
 	g.Manager = gossip.NewManager(g.ProtoComms, gossipFlags)
 
@@ -26,12 +29,79 @@ func (g *Comms) SetUpGossip(gossipFlags gossip.ManagerFlags, receiver gossip.Rec
 // Add newPeer to the list of gossip peers for the given protocol.
 // The protocol is looked up by the given tag. If this protocol is not
 // found then an error is returned
-func (g *Comms) AddGossipPeer(tag string, newPeer *id.ID) error  {
+func (g *Comms) AddGossipPeer(tag string, newPeer *id.ID) error {
 	p, ok := g.Manager.Get(tag)
 	if !ok {
-		return errors.Errorf("Could not find gossip protocol with " +
+		return errors.Errorf("Could not find gossip protocol with "+
 			"tag %s", tag)
 	}
 
 	return p.AddGossipPeer(newPeer)
+}
+
+// gossipBatch builds a gossip message containing all of the sender ID's
+// within the batch and gossips it to all peers
+func (g *Comms) GossipBatch(batch *pb.Batch) []error {
+
+	gossipProtocol, ok := g.Manager.Get("batch")
+	if !ok {
+		return []error{
+			errors.Errorf("Unable to get gossip protocol. " +
+				"Sending batch without gossiping..."),
+		}
+	}
+
+	// Collect all of the sender IDs in the batch
+	var senderIds []*id.ID
+	for i, slot := range batch.Slots {
+		sender, err := id.Unmarshal(slot.SenderID)
+		if err != nil {
+			return []error{errors.Errorf("Could not completely "+
+				"gossip for slot %d in round %d: "+
+				"Unreadable sender ID: %v: %v",
+				i, batch.Round.ID, slot.SenderID, err),
+			}
+		}
+
+		senderIds = append(senderIds, sender)
+	}
+
+	fmt.Printf("senderIDs: %v\n", senderIds)
+
+	// Marshal the list of senders into a json
+	payloadData, err := json.Marshal(senderIds)
+	if err != nil {
+		return []error{
+			errors.Errorf("Could not form gossip payload: %v", err),
+		}
+	}
+
+	var receivedId []*id.ID
+	err = json.Unmarshal(payloadData, &receivedId)
+	if err != nil {
+		return []error{
+			errors.Errorf("Could not marshal sender ID's into a payload: %v", err),
+		}
+	}
+	// Build the message
+	gossipMsg := &gossip.GossipMsg{
+		Tag:     "batch",
+		Origin:  g.Id.Bytes(),
+		Payload: payloadData,
+	}
+
+	// Sign the gateway message
+	sig, err := g.SignMessage(gossipMsg)
+	if err != nil {
+		return []error{
+			errors.Errorf("Could not sign gossip mesage: %v", err),
+		}
+
+	}
+
+	gossipMsg.Signature = sig
+
+	// Gossip the message
+	_, errs := gossipProtocol.Gossip(gossipMsg)
+	return errs
 }
