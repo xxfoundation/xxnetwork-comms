a1e61a9cfffadf37c959fd21fe584de007bbb482
josh <josh@elixxir.io>
2020-09-08 11:01:00 -0700

    Refactor tokens to have internal locks

--- connect/host.go
+++ connect/host.go
@@ -40,7 +40,33 @@ var KaClientOpts = keepalive.ClientParameters{
 }
 
 // Represents a reverse-authentication token
-type Token []byte
+type Token struct {
+	token []byte
+	lock  sync.RWMutex
+}
+
+// Constructor which initializes a token for
+// use by the associated host object
+func NewToken() Token {
+	return Token{
+		token: nil,
+		lock:  sync.RWMutex{},
+	}
+}
+
+// SetToken rewrites the token for negotiation or renegotiation
+func (t *Token) SetToken(newToken []byte)  {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+	t.token = newToken
+}
+
+// GetToken reads and returns the token
+func (t *Token) GetToken() []byte  {
+	t.lock.RLock()
+	defer t.lock.RUnlock()
+	return t.token
+}
 
 // Information used to describe a connection to a host
 type Host struct {
@@ -82,6 +108,9 @@ type Host struct {
 
 	// Read/Write Mutex for thread safety
 	mux sync.RWMutex
+
+	// connection lock
+	sendLock sync.RWMutex
 }
 
 // Creates a new Host object
@@ -90,10 +119,12 @@ func NewHost(id *id.ID, address string, cert []byte, disableTimeout,
 
 	// Initialize the Host object
 	host = &Host{
-		id:          id,
-		address:     address,
-		certificate: cert,
-		enableAuth:  enableAuth,
+		id:                id,
+		address:           address,
+		certificate:       cert,
+		enableAuth:        enableAuth,
+		transmissionToken: NewToken(),
+		receptionToken:    NewToken(),
 	}
 
 	// Set the max number of retries for establishing a connection
@@ -166,7 +197,7 @@ func (h *Host) Disconnect() {
 	defer h.mux.Unlock()
 
 	h.disconnect()
-	h.transmissionToken = nil
+	h.transmissionToken.SetToken(nil)
 }
 
 // Returns whether or not the Host is able to be contacted
@@ -234,7 +265,7 @@ func (h *Host) connect() error {
 	}
 
 	h.disconnect()
-	h.transmissionToken = nil
+	h.transmissionToken.SetToken(nil)
 
 	//connect to remote
 	if err := h.connectHelper(); err != nil {
@@ -250,7 +281,7 @@ func (h *Host) authenticationRequired() bool {
 	h.mux.RLock()
 	defer h.mux.RUnlock()
 
-	return h.enableAuth && h.transmissionToken == nil
+	return h.enableAuth && h.transmissionToken.GetToken() == nil
 }
 
 // Checks if the given Host's connection is alive
@@ -410,8 +441,8 @@ func (h *Host) String() string {
 			"\tMaxRetries: %v\tConnState: %v"+
 			"\tTLS ServerName: %v\tTLS ProtocolVersion: %v\t"+
 			"TLS SecurityVersion: %v\tTLS SecurityProtocol: %v\n",
-		h.id, addr, h.certificate, h.transmissionToken,
-		h.receptionToken, h.enableAuth, h.maxRetries, state,
+		h.id, addr, h.certificate, h.transmissionToken.GetToken(),
+		h.receptionToken.GetToken(), h.enableAuth, h.maxRetries, state,
 		serverName, protocolVersion, securityVersion, securityProtocol)
 }
 
