de56ab7dd92545109cb5d42cf440d2bb04f54bf9
Jake Taylor <jake@elixxir.io>
2020-09-08 16:24:59 -0500

    fix comment

--- gossip/functions.go
+++ gossip/functions.go
@@ -10,10 +10,10 @@
 package gossip
 
 // Passed into NewGossip to specify how Gossip messages will be handled
-// the byte slice will be used to pass in a merkle tree and signature on the
-// trees root for multi-part gossips sent over streaming when streaming is
-// implemented. Ignore it for non streaming implementations.
 type Receiver func(*GossipMsg) error
 
 // Passed into NewGossip to specify how Gossip message signatures will be verified
+// the byte slice will be used to pass in a merkle tree and signature on the
+// trees root for multi-part gossips sent over streaming when streaming is
+// implemented. Ignore it for non streaming implementations.
 type SignatureVerification func(*GossipMsg, []byte) error
