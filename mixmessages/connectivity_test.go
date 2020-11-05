30e42a3110d43666c79dcd419776ed1b0582c6d6
jbhusson <jonah@elixxir.io>
2020-11-05 10:15:46 -0800

    fix connectivity test

--- mixmessages/connectivity_test.go
+++ mixmessages/connectivity_test.go
@@ -1,8 +1,6 @@
 package mixmessages
 
 import (
-	"golang.org/x/net/context"
-	"google.golang.org/grpc/peer"
 	"testing"
 )
 
@@ -16,27 +14,9 @@ import (
 //	}
 //}
 
-// Mock the Addr interface so we can set the address to check for testing
-type MockAddr struct{}
-
-// This does not get used, just fills the interface
-func (m *MockAddr) Network() string {
-	return ""
-}
-
-// Hit an open port on google
-func (m *MockAddr) String() string {
-	return "www.google.com:80"
-}
-
 // Test the functionality & returns of CheckConnectivity
 func TestCheckConnectivity(t *testing.T) {
-	ctx := context.Background()
-	p := &peer.Peer{
-		Addr: &MockAddr{},
-	}
-	ctx = peer.NewContext(ctx, p)
-	resp, err := CheckConnectivity(ctx, "", 0)
+	resp, err := CheckConnectivity("www.google.com", "80", "", "0")
 	if err != nil {
 		t.Errorf("Failed to check connectivity with default params: %+v", err)
 	}
@@ -44,7 +24,7 @@ func TestCheckConnectivity(t *testing.T) {
 		t.Errorf("Did not get expected response for default params: %+v", resp)
 	}
 
-	resp, err = CheckConnectivity(ctx, "", 80)
+	resp, err = CheckConnectivity("www.google.com", "80", "", "80")
 	if err != nil {
 		t.Errorf("Failed to check connectivity with optional port: %+v", err)
 	}
@@ -52,7 +32,7 @@ func TestCheckConnectivity(t *testing.T) {
 		t.Errorf("Did not get expected response for optional port: %+v", resp)
 	}
 
-	resp, err = CheckConnectivity(ctx, "www.google.com", 80)
+	resp, err = CheckConnectivity("www.google.com", "80", "www.google.com", "80")
 	if err != nil {
 		t.Errorf("Failed to check connectivity with optional address: %+v", err)
 	}
