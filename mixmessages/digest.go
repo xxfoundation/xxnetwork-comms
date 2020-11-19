fcf142c9b7f17c807a7f80fd4a572a6b149b20fa
jbhusson <jonah@elixxir.io>
2020-11-19 15:13:44 -0800

    remove fact digest

--- mixmessages/digest.go
+++ mixmessages/digest.go
@@ -25,23 +25,3 @@ func (i *Identity) Digest() []byte {
 	h.Write(mb)
 	return h.Sum(nil)
 }
-
-// Function to digest Fact
-func (i *Fact) Digest() []byte {
-	//return hash(Fact |FactType )}
-	// Generate the hash function
-	h, err := hash.NewCMixHash()
-	if err != nil {
-		jww.FATAL.Panicf("Could not get hash: %+v", err)
-	}
-
-	// Marshal the message to put into the hash
-	mb, err := proto.Marshal(i)
-	if err != nil {
-		jww.FATAL.Panicf("Could not marshal: %+v", err)
-	}
-
-	// Hash the Fact data to generate the vector
-	h.Write(mb)
-	return h.Sum(nil)
-}
