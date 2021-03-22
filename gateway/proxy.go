1ad402f1dab7e4779ed36e8c327d08c0615b729d
Jono Wenger <jono@elixxir.io>
2021-03-22 11:50:57 -0700

    Add proxy send functions to forward message to gateway

---
+++ gateway/proxy.go
@@ -0,0 +1,141 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright © 2020 xx network SEZC                                          //
+//                                                                           //
+// Use of this source code is governed by a license that can be found in the //
+// LICENSE file                                                              //
+///////////////////////////////////////////////////////////////////////////////
+
+package gateway
+
+import (
+	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/any"
+	jww "github.com/spf13/jwalterweatherman"
+	pb "gitlab.com/elixxir/comms/mixmessages"
+	"gitlab.com/xx_network/comms/connect"
+	"google.golang.org/grpc"
+)
+
+// Gateway -> Gateway forward client PutMessage.
+func (g *Comms) SendPutMessage(host *connect.Host,
+	messages *pb.GatewaySlot) (*pb.GatewaySlotResponse, error) {
+
+	// Create the Send Function
+	f := func(conn *grpc.ClientConn) (*any.Any, error) {
+		// Set up the context
+		ctx, cancel := connect.MessagingContext()
+		defer cancel()
+
+		// Send the message
+		resultMsg, err := pb.NewGatewayClient(conn).PutMessage(ctx, messages)
+		if err != nil {
+			return nil, err
+		}
+
+		return ptypes.MarshalAny(resultMsg)
+	}
+
+	// Execute the Send function
+	jww.TRACE.Printf("Sending client PutMessage: %+v", messages)
+	resultMsg, err := g.Send(host, f)
+	if err != nil {
+		return nil, err
+	}
+
+	// Marshall the result
+	result := &pb.GatewaySlotResponse{}
+	return result, ptypes.UnmarshalAny(resultMsg, result)
+}
+
+// Gateway -> Gateway forward client RequestNonce.
+func (g *Comms) SendRequestNonce(host *connect.Host,
+	messages *pb.NonceRequest) (*pb.Nonce, error) {
+
+	// Create the Send Function
+	f := func(conn *grpc.ClientConn) (*any.Any, error) {
+		// Set up the context
+		ctx, cancel := connect.MessagingContext()
+		defer cancel()
+
+		// Send the message
+		resultMsg, err := pb.NewGatewayClient(conn).RequestNonce(ctx, messages)
+		if err != nil {
+			return nil, err
+		}
+
+		return ptypes.MarshalAny(resultMsg)
+	}
+
+	// Execute the Send function
+	jww.TRACE.Printf("Sending client RequestNonce: %+v", messages)
+	resultMsg, err := g.Send(host, f)
+	if err != nil {
+		return nil, err
+	}
+
+	// Marshall the result
+	result := &pb.Nonce{}
+	return result, ptypes.UnmarshalAny(resultMsg, result)
+}
+
+// Gateway -> Gateway forward client ConfirmNonce.
+func (g *Comms) SendConfirmNonce(host *connect.Host,
+	messages *pb.RequestRegistrationConfirmation) (*pb.RegistrationConfirmation, error) {
+
+	// Create the Send Function
+	f := func(conn *grpc.ClientConn) (*any.Any, error) {
+		// Set up the context
+		ctx, cancel := connect.MessagingContext()
+		defer cancel()
+
+		// Send the message
+		resultMsg, err := pb.NewGatewayClient(conn).ConfirmNonce(ctx, messages)
+		if err != nil {
+			return nil, err
+		}
+
+		return ptypes.MarshalAny(resultMsg)
+	}
+
+	// Execute the Send function
+	jww.TRACE.Printf("Sending client ConfirmNonce: %+v", messages)
+	resultMsg, err := g.Send(host, f)
+	if err != nil {
+		return nil, err
+	}
+
+	// Marshall the result
+	result := &pb.RegistrationConfirmation{}
+	return result, ptypes.UnmarshalAny(resultMsg, result)
+}
+
+// Gateway -> Gateway forward client RequestMessages.
+func (g *Comms) SendRequestMessages(host *connect.Host,
+	messages *pb.GetMessages) (*pb.GetMessagesResponse, error) {
+
+	// Create the Send Function
+	f := func(conn *grpc.ClientConn) (*any.Any, error) {
+		// Set up the context
+		ctx, cancel := connect.MessagingContext()
+		defer cancel()
+
+		// Send the message
+		resultMsg, err := pb.NewGatewayClient(conn).RequestMessages(ctx, messages)
+		if err != nil {
+			return nil, err
+		}
+
+		return ptypes.MarshalAny(resultMsg)
+	}
+
+	// Execute the Send function
+	jww.TRACE.Printf("Sending client RequestMessages: %+v", messages)
+	resultMsg, err := g.Send(host, f)
+	if err != nil {
+		return nil, err
+	}
+
+	// Marshall the result
+	result := &pb.GetMessagesResponse{}
+	return result, ptypes.UnmarshalAny(resultMsg, result)
+}
