e0f969b6f401a02a51700d5a5fade84aa9bde3ba
josh <josh@elixxir.io>
2021-08-12 09:09:58 -0700

    Remove bad test

--- registration/registration_test.go
+++ registration/registration_test.go
@@ -9,10 +9,6 @@ package registration
 
 import (
 	"fmt"
-	"gitlab.com/elixxir/comms/client"
-	pb "gitlab.com/elixxir/comms/mixmessages"
-	"gitlab.com/elixxir/comms/testkeys"
-	"gitlab.com/xx_network/comms/connect"
 	"gitlab.com/xx_network/primitives/id"
 	"sync"
 	"testing"
@@ -28,37 +24,6 @@ func getNextServerAddress() string {
 		serverPortLock.Unlock()
 	}()
 	return fmt.Sprintf("localhost:%d", serverPort)
-}
-
-// Tests whether the server can be connected to and run an RPC with TLS enabled
-func TestTLS(t *testing.T) {
-	RegAddress := getNextServerAddress()
-
-	keyPath := testkeys.GetNodeKeyPath()
-	keyData := testkeys.LoadFromPath(keyPath)
-	certPath := testkeys.GetNodeCertPath()
-	certData := testkeys.LoadFromPath(certPath)
-	testId := id.NewIdFromString("test", id.Generic, t)
-
-	rg := StartRegistrationServer(testId, RegAddress, NewImplementation(), certData, keyData, nil)
-	// Well, client shouldn't have a server type because it's not a server
-	// It's a client
-	// So, we need some way to add a connection to the manager for the client
-	defer rg.Shutdown()
-	var c client.Comms
-	manager := connect.NewManagerTesting(t)
-
-	params := connect.GetDefaultHostParams()
-	params.AuthEnabled = false
-	host, err := manager.AddHost(testId, RegAddress, certData, params)
-	if err != nil {
-		t.Errorf("Unable to call NewHost: %+v", err)
-	}
-
-	_, err = c.SendRegistrationMessage(host, &pb.UserRegistration{})
-	if err != nil {
-		t.Errorf("RegistrationMessage: Error received: %s", err)
-	}
 }
 
 func TestBadCerts(t *testing.T) {
