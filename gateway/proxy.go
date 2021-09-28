6503f042eaf378a1dc8e6f9fd4ff37e7a5e7401e
josh <josh@elixxir.io>
2021-09-28 15:15:16 -0700

    Add proxy fo rdepreacted Nonce registration

--- gateway/proxy.go
+++ gateway/proxy.go
@@ -18,6 +18,105 @@ import (
 	"google.golang.org/grpc"
 )
 
+// ---------------------- Start of deprecated fields ----------- //
+
+// TODO: Remove comm once RequestClientKey is properly tested
+// Gateway -> Gateway forward client RequestNonce.
+func (g *Comms) SendRequestNonce(host *connect.Host,
+	messages *pb.NonceRequest) (*pb.Nonce, error) {
+
+	// Create the Send Function
+	f := func(conn *grpc.ClientConn) (*any.Any, error) {
+		// Set up the context
+		ctx, cancel := host.GetMessagingContext()
+		defer cancel()
+
+		// Send the message
+		resultMsg, err := pb.NewGatewayClient(conn).RequestNonce(ctx, messages)
+		if err != nil {
+			return nil, err
+		}
+
+		return ptypes.MarshalAny(resultMsg)
+	}
+
+	// Execute the Send function
+	jww.TRACE.Printf("Sending client RequestNonce: %+v", messages)
+	resultMsg, err := g.Send(host, f)
+	if err != nil {
+		return nil, err
+	}
+
+	// Marshall the result
+	result := &pb.Nonce{}
+	return result, ptypes.UnmarshalAny(resultMsg, result)
+}
+
+// Gateway -> Gateway forward client ConfirmNonce.
+// TODO: Remove comm once RequestClientKey is properly tested
+func (g *Comms) SendConfirmNonce(host *connect.Host,
+	messages *pb.RequestRegistrationConfirmation) (*pb.RegistrationConfirmation, error) {
+
+	// Create the Send Function
+	f := func(conn *grpc.ClientConn) (*any.Any, error) {
+		// Set up the context
+		ctx, cancel := host.GetMessagingContext()
+		defer cancel()
+
+		// Send the message
+		resultMsg, err := pb.NewGatewayClient(conn).ConfirmNonce(ctx, messages)
+		if err != nil {
+			return nil, err
+		}
+
+		return ptypes.MarshalAny(resultMsg)
+	}
+
+	// Execute the Send function
+	jww.TRACE.Printf("Sending client ConfirmNonce: %+v", messages)
+	resultMsg, err := g.Send(host, f)
+	if err != nil {
+		return nil, err
+	}
+
+	// Marshall the result
+	result := &pb.RegistrationConfirmation{}
+	return result, ptypes.UnmarshalAny(resultMsg, result)
+}
+
+// ---------------------- End of deprecated fields ----------- //
+
+// Gateway -> Gateway forward client RequestNonce.
+func (g *Comms) SendRequestClientKey(host *connect.Host,
+	messages *pb.SignedClientKeyRequest) (*pb.SignedKeyResponse, error) {
+
+	// Create the Send Function
+	f := func(conn *grpc.ClientConn) (*any.Any, error) {
+		// Set up the context
+		ctx, cancel := host.GetMessagingContext()
+		defer cancel()
+
+		// Send the message
+		resultMsg, err := pb.NewGatewayClient(conn).RequestClientKey(ctx, messages)
+		if err != nil {
+			return nil, err
+		}
+
+		return ptypes.MarshalAny(resultMsg)
+	}
+
+	// Execute the Send function
+	jww.TRACE.Printf("Sending client RequestNonce: %+v", messages)
+	resultMsg, err := g.Send(host, f)
+	if err != nil {
+		return nil, err
+	}
+
+	// Marshall the result
+	result := &pb.SignedKeyResponse{}
+	return result, ptypes.UnmarshalAny(resultMsg, result)
+}
+
 // Gateway -> Gateway forward client PutMessage.
 func (g *Comms) SendPutMessage(host *connect.Host,
 	messages *pb.GatewaySlot) (*pb.GatewaySlotResponse, error) {
@@ -77,68 +176,6 @@ func (g *Comms) SendPutManyMessages(host *connect.Host,
 
 	// Marshall the result
 	result := &pb.GatewaySlotResponse{}
-	return result, ptypes.UnmarshalAny(resultMsg, result)
-}
-
-// Gateway -> Gateway forward client RequestNonce.
-func (g *Comms) SendRequestClientKey(host *connect.Host,
-	messages *pb.SignedClientKeyRequest) (*pb.SignedKeyResponse, error) {
-
-	// Create the Send Function
-	f := func(conn *grpc.ClientConn) (*any.Any, error) {
-		// Set up the context
-		ctx, cancel := host.GetMessagingContext()
-		defer cancel()
-
-		// Send the message
-		resultMsg, err := pb.NewGatewayClient(conn).RequestClientKey(ctx, messages)
-		if err != nil {
-			return nil, err
-		}
-
-		return ptypes.MarshalAny(resultMsg)
-	}
-
-	// Execute the Send function
-	jww.TRACE.Printf("Sending client RequestNonce: %+v", messages)
-	resultMsg, err := g.Send(host, f)
-	if err != nil {
-		return nil, err
-	}
-
-	// Marshall the result
-	result := &pb.SignedKeyResponse{}
-	return result, ptypes.UnmarshalAny(resultMsg, result)
-}
-
-// Gateway -> Gateway forward client ConfirmNonce.
-func (g *Comms) SendConfirmNonce(host *connect.Host,
-	messages *pb.RequestRegistrationConfirmation) (*pb.RegistrationConfirmation, error) {
-
-	// Create the Send Function
-	f := func(conn *grpc.ClientConn) (*any.Any, error) {
-		// Set up the context
-		ctx, cancel := host.GetMessagingContext()
-		defer cancel()
-
-		// Send the message
-		resultMsg, err := pb.NewGatewayClient(conn).ConfirmNonce(ctx, messages)
-		if err != nil {
-			return nil, err
-		}
-
-		return ptypes.MarshalAny(resultMsg)
-	}
-
-	// Execute the Send function
-	jww.TRACE.Printf("Sending client ConfirmNonce: %+v", messages)
-	resultMsg, err := g.Send(host, f)
-	if err != nil {
-		return nil, err
-	}
-
-	// Marshall the result
-	result := &pb.RegistrationConfirmation{}
 	return result, ptypes.UnmarshalAny(resultMsg, result)
 }
 
