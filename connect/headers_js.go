5d9ef9b076d0246d1d87de5ca2c5d1fab196baef
jbhusson <jonah@elixxir.io>
2023-01-04 19:01:13 -0500

    Fix headers_js

--- connect/headers_js.go
+++ connect/headers_js.go
@@ -5,6 +5,6 @@ package connect
 import "net/http"
 
 func (wc *webConn) addHeaders(header http.Header) http.Header {
-	req.Header.Add("js.fetch:mode", "no-cors")
+	header.Add("js.fetch:mode", "no-cors")
 	return header
 }
